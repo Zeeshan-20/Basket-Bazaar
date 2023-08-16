4eaf05a5f5413a9fd2b8946168f03a6dc682e4db
Zeeshan Khan <zeeshankhn00786@gmail.com>
2023-08-17 00:32:53 +0530

    Create Product     1. create products button     2. implement backend api     3. handle on click

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,6 +9,29 @@ productRouter.get('/', async (req, res) => {
     const products = await Product.find();
     res.send(products);
 });
+
+// Create Product
+productRouter.post(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const newProduct = new Product({
+      name: 'sample name ' + Date.now(),
+      slug: 'sample-name-' + Date.now(),
+      image: '/images/p1.jpg',
+      price: 0,
+      category: 'sample category',
+      brand: 'sample brand',
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: 'sample description',
+    });
+    const product = await newProduct.save();
+    res.send({ message: 'Product Created', product });
+  })
+);
 // Create Search Screen
 // 1. show filters
 // 2. create api for searching products
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,9 +1,12 @@
 import React, { useContext, useEffect, useReducer } from 'react';
 import axios from 'axios';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Store } from '../store';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { Button, Col, Row } from 'react-bootstrap';
+import { getError } from '../utils';
+import { toast } from 'react-toastify';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -20,19 +23,38 @@ const reducer = (state, action) => {
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
 
+    //   Create Product
+    //  create products button
+    case 'CREATE_REQUEST':
+      return { ...state, loadingCreate: true };
+    case 'CREATE_SUCCESS':
+      return {
+        ...state,
+        loadingCreate: false,
+      };
+    case 'CREATE_FAIL':
+      return { ...state, loadingCreate: false };
+
+    // Create Product end
+
     default:
       return state;
   }
 };
 
 export default function ProductListScreen() {
-  const [{ loading, error, products, pages }, dispatch] = useReducer(reducer, {
-    loading: true,
-    error: '',
-  });
-// eslint-disable-next-line
-  const { search, pathname } = useLocation();
-  
+  // Create Product
+
+  const [{ loading, error, products, pages, loadingCreate }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: '',
+    });
+  // eslint-disable-next-line
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  // Create Product
+
   const sp = new URLSearchParams(search);
   const page = sp.get('page') || 1;
 
@@ -47,14 +69,58 @@ export default function ProductListScreen() {
         });
 
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
-      } catch (err) {}
+      } catch (err) { }
     };
     fetchData();
   }, [page, userInfo]);
 
+  // Create Product
+  //    create products button
+  const createHandler = async () => {
+    if (window.confirm('Are you sure to create?')) {
+      try {
+        dispatch({ type: 'CREATE_REQUEST' });
+        const { data } = await axios.post(
+          '/api/products',
+          {},
+          {
+            headers: { Authorization: `Bearer ${userInfo.token}` },
+          }
+        );
+        toast.success('product created successfully');
+        dispatch({ type: 'CREATE_SUCCESS' });
+        navigate(`/admin/product/${data.product._id}`);
+      } catch (err) {
+        toast.error(getError(error));
+        dispatch({
+          type: 'CREATE_FAIL',
+        });
+      }
+    }
+  };
+  // Create Product end
+    
+
   return (
     <div>
-      <h1>Products</h1>
+      {/* Create Product
+    create products button */}
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="col text-end">
+          <div>
+            <Button type="button" onClick={createHandler}>
+              Create Product
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      {/* Create Product
+    create products button end */}
+
+      {loadingCreate && <LoadingBox></LoadingBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
