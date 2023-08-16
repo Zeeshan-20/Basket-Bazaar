d81d340dfe4e30713dc5029b47871d9430eca494
Zeeshan Khan <zeeshankhn00786@gmail.com>
2023-08-16 23:30:22 +0530

    Create Dashboard Screen     1. create dashboard ui     2. implement backend api     3. connect ui to backend

--- backend/utils.js
+++ backend/utils.js
@@ -27,3 +27,11 @@ export const isAuth = (req, res, next) => {
       res.status(401).send({ message: 'No Token' });
     }
   };
+  // implement backend api Dashboard Screen
+  export const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.status(401).send({ message: 'Invalid Admin Token' });
+    }
+  };
