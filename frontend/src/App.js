0372b88ecd4d684dbcea167ea7f9d67e668985c0
Zeeshan Khan <zeeshankhn00786@gmail.com>
2023-08-17 17:57:14 +0530

    List Orders     1. create order list screen     2. implement backen api     3. fetch and display orders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,7 @@ import DashboardScreen from "./screens/DashboardScreen";
 import AdminRoute from "./components/AdminRoute";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -183,6 +184,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/orders"
+                element={
+                  <AdminRoute>
+                    <OrderListScreen />
                   </AdminRoute>
                 }
               ></Route>
